d601e3a5a659356c640bf9d801f086ae880caeef
Nilesh Shah <nileshshah@Nileshs-MacBook-Pro.local>
2016-07-03 21:58:46 -0700

    more comments

--- blocktools.py
+++ blocktools.py
@@ -23,6 +23,7 @@ def uint1(stream):
 	# ...     print(blockfile.read(20)[::-2])
 	# ... 
 	# nljhgeca
+	#f.tell() returns an integer giving the file object’s current position in the file, measured in bytes from the beginning of the file. To change the file object’s position, use f.seek(offset, from_what). The position is computed from adding offset to a reference point; the reference point is selected by the from_what argument. A from_what value of 0 measures from the beginning of the file, 1 uses the current file position, and 2 uses the end of the file as the reference point. from_what can be omitted and defaults to 0, using the beginning of the file as the reference point.
 
 
 	return ord(stream.read(1))
